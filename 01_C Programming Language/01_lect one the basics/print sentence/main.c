87091232018dcd5394a525696c1daf659c920cb7
magdy edwar farid <70660386+Magdyedwar1996@users.noreply.github.com>
2021-01-23 23:44:34 +0200

    this code is for printing a sentence

---
+++ 01_C Programming Language/01_lect one the basics/print sentence/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 23, 2021
+ *      Author: magdy
+ */
+
+//Write C Program to Print a Sentence
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("##########Console-output### \n");
+printf("C Programming\n");
+printf("###########################\n");
+    return 0;
+}
+
+
+
