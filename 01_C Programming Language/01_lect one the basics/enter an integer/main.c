c62fe3e8395b2cb58ead7a68debc3e99341a7f51
magdy edwar farid <70660386+Magdyedwar1996@users.noreply.github.com>
2021-01-24 00:01:47 +0200

    code for enter an integer

---
+++ 01_C Programming Language/01_lect one the basics/enter an integer/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 23, 2021
+ *      Author: magdy
+ */
+
+
+//Write C Program to Print a Integer Entered by a User
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int num ;
+printf("enter an integer: \n");
+scanf("%d", &num );
+printf("you entered %d", num );
+    return 0;
+}
+
+
