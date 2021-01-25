a73c11c0f2545b7befe69ab9221c14ffeafb1543
magdy edwar farid <70660386+Magdyedwar1996@users.noreply.github.com>
2021-01-25 17:52:25 +0200

    this code is to find the ASCII value of the charchater

---
+++ 01_C Programming Language/01_lect one the basics/ASCII Code/main.c
@@ -0,0 +1,14 @@
+
+
+//Write C Program to Find ASCII Value of a Character
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ char character ;
+    printf("Enter a character : \n");
+    scanf("%c",&character );
+    printf("the ASCII is : %d", character);
+    return 0;
+}
